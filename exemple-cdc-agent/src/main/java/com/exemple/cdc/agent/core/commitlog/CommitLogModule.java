8575e5d5edc2ee07e9e77086511af121c487f68b
doudouchat <baraultb@hotmail.com>
2023-12-13 23:08:14 +0000

    improve dependency injection

--- exemple-cdc-agent/src/main/java/com/exemple/cdc/agent/core/commitlog/CommitLogModule.java
+++ exemple-cdc-agent/src/main/java/com/exemple/cdc/agent/core/commitlog/CommitLogModule.java
@@ -1,5 +1,6 @@
 package com.exemple.cdc.agent.core.commitlog;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.apache.cassandra.db.commitlog.CommitLogReadHandler;
@@ -7,12 +8,20 @@ import org.apache.cassandra.db.commitlog.CommitLogReader;
 
 import com.exemple.cdc.agent.commitlog.CommitLogReadHandlerImpl;
 import com.exemple.cdc.agent.commitlog.CommitLogReaderImpl;
+import com.exemple.cdc.agent.event.EventProducer;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class CommitLogModule {
+
+    private final EventProducer eventProducer;
+
+    @Inject
+    public CommitLogModule(EventProducer eventProducer) {
+        this.eventProducer = eventProducer;
+    }
 
     @Provides
     @Singleton
@@ -23,7 +32,7 @@ public class CommitLogModule {
     @Provides
     @Singleton
     public CommitLogReadHandler commitLogReadHandler() {
-        return new CommitLogReadHandlerImpl();
+        return new CommitLogReadHandlerImpl(eventProducer);
     }
 
 }
--- exemple-cdc-agent/src/main/java/com/exemple/cdc/agent/event/EventProducer.java
+++ exemple-cdc-agent/src/main/java/com/exemple/cdc/agent/event/EventProducer.java
@@ -14,16 +14,14 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.zookeeper.CreateMode;
 
 import com.exemple.cdc.agent.common.CdcEvent;
-import com.exemple.cdc.agent.core.kafka.DaggerKafkaProducerComponent;
-import com.exemple.cdc.agent.core.kafka.KafkaProducerModule;
 import com.exemple.cdc.agent.core.kafka.KafkaProperties;
-import com.exemple.cdc.agent.core.zookeeper.DaggerZookeeperClientComponent;
-import com.exemple.cdc.agent.core.zookeeper.ZookeeperClientModule;
 import com.exemple.cdc.agent.core.zookeeper.ZookeeperProperties;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
+@RequiredArgsConstructor
 public class EventProducer {
 
     public static final String X_ORIGIN = "X_Origin";
@@ -36,25 +34,11 @@ public class EventProducer {
 
     private final Producer<String, JsonNode> kafkaProducer;
 
-    private final CuratorFramework zookeeperClient;
-
     private final KafkaProperties kafkaProperties;
 
+    private final CuratorFramework zookeeperClient;
+
     private final ZookeeperProperties zookeeperProperties;
-
-    public EventProducer(String path) {
-        var kafkaProducerComponent = DaggerKafkaProducerComponent.builder().kafkaProducerModule(new KafkaProducerModule(path)).build();
-        this.kafkaProducer = kafkaProducerComponent.kafkaProducer();
-        this.kafkaProperties = kafkaProducerComponent.kafkaProperties();
-
-        var zookeeperClientComponent = DaggerZookeeperClientComponent.builder().zookeeperClientModule(new ZookeeperClientModule(path)).build();
-        this.zookeeperClient = zookeeperClientComponent.zookeeperClient();
-        this.zookeeperProperties = zookeeperClientComponent.zookeeperProperties();
-    }
-
-    public EventProducer() {
-        this("/tmp/conf/exemple-cdc.yml");
-    }
 
     @SneakyThrows
     public void send(CdcEvent event) {
