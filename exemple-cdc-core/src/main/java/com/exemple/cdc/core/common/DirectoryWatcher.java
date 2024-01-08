1bea05e6cc9e9e124b6b01c2793dc8e973c4b5f7
doudouchat <baraultb@hotmail.com>
2024-01-08 19:37:33 +0000

    fix commit log create before agent

--- exemple-cdc-core/src/main/java/com/exemple/cdc/core/common/DirectoryWatcher.java
+++ exemple-cdc-core/src/main/java/com/exemple/cdc/core/common/DirectoryWatcher.java
@@ -3,39 +3,72 @@ package com.exemple.cdc.core.common;
 import java.io.File;
 import java.io.FileFilter;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
 import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public final class DirectoryWatcher {
 
-    @SneakyThrows
-    public static void onCreateFile(Path directory, long intervalMillis, Consumer<File> onChangeOrCreate) {
+    private final Path directory;
 
-        onCreateFile(directory, intervalMillis, f -> true, onChangeOrCreate);
-    }
+    private final long intervalMillis;
+
+    private final FileFilter filter;
+
+    private final Consumer<File> consumerFile;
+
+    private final Set<String> runningConsumer = new HashSet<>();
 
     @SneakyThrows
-    public static void onCreateFile(Path directory, long intervalMillis, FileFilter filter, Consumer<File> onChangeOrCreate) {
+    public void onCreateFile() {
 
         var observer = new FileAlterationObserver(directory.toString(), filter);
         var monitor = new FileAlterationMonitor(intervalMillis);
         var listener = new FileAlterationListenerAdaptor() {
+
+            @Override
+            public void onStop(FileAlterationObserver observer) {
+
+                Arrays.stream(observer.getDirectory().listFiles(filter)).forEach(file -> accept(file));
+            }
+
             @Override
             public void onFileCreate(File file) {
-                onChangeOrCreate.accept(file);
+
+                accept(file);
+            }
+
+            @Override
+            public void onFileDelete(File file) {
+
+                runningConsumer.remove(file.getName());
             }
         };
         observer.addListener(listener);
         monitor.addObserver(observer);
         monitor.start();
+    }
+
+    private void accept(File file) {
+
+        synchronized (this) {
+            if (runningConsumer.contains(file.getName())) {
+                return;
+
+            }
+            runningConsumer.add(file.getName());
+        }
+
+        consumerFile.accept(file);
     }
 
 }
